bde0b275b70b3ee29cd688a95b87cd1f4bd9e90b
mateusz_dominik_piotrowski <mateusz.piotrowski@apricot.com.pl>
2020-03-30 17:52:04 +0200

    "Settings" screen design design implementation

--- app/src/main/java/com/mpiotrowski/maudiofasttrackmixer/ui/fragments/SettingsFragment.kt
+++ app/src/main/java/com/mpiotrowski/maudiofasttrackmixer/ui/fragments/SettingsFragment.kt
@@ -1,9 +1,11 @@
 package com.mpiotrowski.maudiofasttrackmixer.ui.fragments
 
+import android.R
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import androidx.fragment.app.Fragment
 import com.mpiotrowski.maudiofasttrackmixer.databinding.FragmentSettingsBinding
 
@@ -14,6 +16,19 @@ class SettingsFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         viewDataBinding = FragmentSettingsBinding.inflate(inflater, container, false)
+
+        //TODO replace with actual ViewModel
+        var effectTypeValues = arrayOf("Room1","Room2","Room3","Hall1","Hall2","Plate","Delay","Echo")
+        val effectTypeAdapter = ArrayAdapter(requireContext(), R.layout.simple_spinner_item, effectTypeValues)
+        effectTypeAdapter.setDropDownViewResource(R.layout.simple_spinner_dropdown_item)
+        viewDataBinding.spinnerEffectType.adapter = effectTypeAdapter
+
+        var sampleRateeValues = arrayOf("44.1kHZ","48kHz","96kHz")
+        val sampleRateAdapter = ArrayAdapter(requireContext(), R.layout.simple_spinner_item, sampleRateeValues)
+        sampleRateAdapter.setDropDownViewResource(R.layout.simple_spinner_dropdown_item)
+        viewDataBinding.spinnerSampleRate.adapter = sampleRateAdapter
+
+
         return viewDataBinding.root
     }
 
