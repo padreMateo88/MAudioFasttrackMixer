f5aaad8f57798cc170a940f5b88e7a803743b819
Waclaw Szablowski <waclaw.szablowski@apricot.com.pl>
2020-04-16 19:39:50 +0200

    added bottomNavigation using jetpack's navigation feature instead of viewpager navigation

--- app/src/main/java/com/mpiotrowski/maudiofasttrackmixer/ui/MainActivity.kt
+++ app/src/main/java/com/mpiotrowski/maudiofasttrackmixer/ui/MainActivity.kt
@@ -2,8 +2,11 @@ package com.mpiotrowski.maudiofasttrackmixer.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.viewpager.widget.ViewPager
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.ui.setupWithNavController
 import com.mpiotrowski.maudiofasttrackmixer.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -11,9 +14,11 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val adapter = MainViewPagerAdapter(supportFragmentManager)
-        val viewPager: ViewPager = findViewById(R.id.viewPager)
-        viewPager.adapter = adapter
-        viewPager.currentItem = 1
+        val navController = Navigation.findNavController(this, R.id.nav_host_fragment)
+        setupBottomNavMenu(navController)
+    }
+
+    private fun setupBottomNavMenu(navController: NavController) {
+        bottom_nav.setupWithNavController(navController)
     }
 }
