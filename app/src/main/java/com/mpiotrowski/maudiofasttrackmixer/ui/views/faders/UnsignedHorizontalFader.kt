1ba1aac2de6efe040438ea6e619383760f5f0d38
mateusz_dominik_piotrowski <mateusz.piotrowski@apricot.com.pl>
2020-05-02 17:19:02 +0200

    FxSettings fader

--- app/src/main/java/com/mpiotrowski/maudiofasttrackmixer/ui/views/faders/UnsignedHorizontalFader.kt
+++ app/src/main/java/com/mpiotrowski/maudiofasttrackmixer/ui/views/faders/UnsignedHorizontalFader.kt
@@ -6,7 +6,7 @@ import android.graphics.Color
 import android.graphics.Paint
 import android.util.AttributeSet
 
-class UnsignedHorizontalFader @JvmOverloads constructor(
+open class UnsignedHorizontalFader @JvmOverloads constructor(
     context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
 ) : HorizontalFader(context, attrs, defStyleAttr) {
 
@@ -31,11 +31,12 @@ class UnsignedHorizontalFader @JvmOverloads constructor(
         paint.style = Paint.Style.FILL
 
         val halfKnobWidth = (scaledBitmap?.width ?: 0)/2f
-        for (i in 4 .. 30 step 4)
+        for (i in 4 .. 28 step 4)
             canvas?.drawLine(width*(36-i)/32f - halfKnobWidth,height*(i/4f)/20f,width*(36-i)/32f - halfKnobWidth,height*(20-i/4f)/20f,paint)
+
 
         paint.strokeWidth = 3f
         paint.color = Color.BLACK
-        canvas?.drawLine(width*0.1f,height/2f,width*0.9f,height/2f,paint)
+        canvas?.drawLine(halfKnobWidth,height/2f,width - halfKnobWidth,height/2f,paint)
     }
 }
